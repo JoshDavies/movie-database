417614aa9d736805fe8a472e3ae7e9bf51c1ddea
JoshDavies <devjoshdavies@gmail.com>
2021-04-25 21:17:44 +0100

    created a button that can toggle the css class of the html doc. and set up a dry way to have multiple themes using scss mixins and maps

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -3,16 +3,27 @@ import './HomePage.scss';
 import Button from 'Components/Button'
 
 const HomePage = () => {
+
+    const toggleTheme = (): void => {
+        let currentTheme: string = document.documentElement.className;
+        document.documentElement.className = '';
+        if (currentTheme === 'theme-light') {
+            document.documentElement.classList.add(`theme-${'dark'}`);
+        } else {
+            document.documentElement.classList.add(`theme-${'light'}`);
+        }
+    }
+
     return (
         <div id='HomePage'>
             <div className='container' >
                 <h2>Home Page</h2>
                 <p>Welcome to the Home Page.</p>
                 <Button 
-                    id={'newButton'}
+                    id={'toggleThemeButton'}
                     type={'button'}
-                    onClick={()=> {}}
-                    text={'Click Me!'}
+                    onClick={()=> {toggleTheme()}}
+                    text={'Change theme'}
                 />
             </div>
         </div>
