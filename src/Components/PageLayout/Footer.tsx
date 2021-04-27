d106a137ace098587c514eb52bdb0a9166b3a63e
JoshDavies <devjoshdavies@gmail.com>
2021-04-27 17:39:52 +0100

    moved link to footer

--- src/Components/PageLayout/Footer.tsx
+++ src/Components/PageLayout/Footer.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => (
-    <footer className='footer container' id ='footer' role='contentinfo'>
-      <p>Project by:{' '}
-        <a
-          className="repo-link"
-          href="https://github.com/JoshDavies/"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label='visit Josh Davies GitHub Profile'
-        >
-          <FontAwesomeIcon icon={['fab', 'github-square']} /> Josh Davies
-        </a>
-      </p>
-    </footer>
+  <footer className='footer container' id ='footer' role='contentinfo'>
+    <p>Project by:{' '}
+      <a
+        className="repo-link"
+        href="https://github.com/JoshDavies/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label='visit Josh Davies GitHub Profile'
+      >
+        <FontAwesomeIcon icon={['fab', 'github-square']} /> Josh Davies
+      </a>
+    </p>
+    <p>
+      <a
+        className="repo-link"
+        href="https://github.com/JoshDavies/theme-builder/tree/main"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github Repo
+      </a>
+    </p>
+  </footer>
 );
 
 export default Header;
