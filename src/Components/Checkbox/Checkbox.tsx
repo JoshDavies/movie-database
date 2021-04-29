176f228e2aa1cf8d44720c565fde12d1d87f05f3
JoshDavies <devjoshdavies@gmail.com>
2021-04-29 20:20:44 +0100

    added a checkbox component, with accessability consideration, placeholder colors for now. not theme aware. checkboxes state hard coded for now

---
+++ src/Components/Checkbox/Checkbox.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Checkbox.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+type propTypes = {
+    id: string,
+    isSelected: boolean,
+    toggleCheckbox: () => void,
+    labelText: string,
+    isDisabled?: boolean,
+    altCheckColor?: string,
+}
+
+const Checkbox = ({
+    id, isSelected, toggleCheckbox, labelText, isDisabled = false, altCheckColor, 
+}: propTypes) => {
+    return (
+        <span className='checkboxComponent'>
+            <button 
+                id = {`checkbox-${id}`} 
+                type = {'button'}
+                className = 'checkbox'
+                onClick = {toggleCheckbox} 
+                onKeyPress = {toggleCheckbox} 
+                disabled = {isDisabled}
+                role = 'checkbox' 
+                aria-checked = {isSelected}
+                aria-labelledby = {`checkbox-label-${id}`} 
+            >
+                {isSelected &&
+                    <FontAwesomeIcon icon='check' className = {'check ' + altCheckColor}/>
+                }
+            </button>
+            <label 
+                htmlFor = {`checkbox-label-${id}`}
+                onClick = {toggleCheckbox} 
+                onKeyPress = {toggleCheckbox} 
+                className = 'checkboxLabel'
+            >
+                {labelText}
+            </label>
+        </span>
+    )
+};
+
+export default Checkbox;
