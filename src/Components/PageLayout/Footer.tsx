0983dc489cb3ed25122be4cf6d92644a94be1860
JoshDavies <devjoshdavies@gmail.com>
2021-05-10 22:24:43 +0100

    adding the mdb logo to the footer as part of the terms of use

--- src/Components/PageLayout/Footer.tsx
+++ src/Components/PageLayout/Footer.tsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import { observer } from "mobx-react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import logo from 'logo.svg'; 
+import reactLogo from 'logo.svg'; 
+import movieDataBaseLogo from 'movieDatabaseLogo.svg';
 
 const Footer = () => (
   <footer className='footer container' id ='footer' role='contentinfo'>
     <FontAwesomeIcon icon={['fab', 'github']} size='3x'/>
     <span>
-    <p>Project:{' '}
-      <a
-        className="repo-link"
-        href="https://github.com/JoshDavies/movie-database"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Github Repo
-      </a>
-    </p>
-    <p>Author:{' '}
-      <a
-        className="repo-link"
-        href="https://github.com/JoshDavies/"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label='visit Josh Davies GitHub Profile'
-      >
-        Josh Davies
-      </a>
-    </p>
+      <p>Project:{' '}
+        <a
+          className="repo-link"
+          href="https://github.com/JoshDavies/movie-database"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Repo
+        </a>
+      </p>
+      <p>Author:{' '}
+        <a
+          className="repo-link"
+          href="https://github.com/JoshDavies/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label='visit Josh Davies GitHub Profile'
+        >
+          Josh Davies
+        </a>
+      </p>
+      <img src={movieDataBaseLogo} className="movieDatabaseLogo" alt="movie database logo" />
+      <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
     </span>
-    <img src={logo} className="App-logo" alt="logo" />
+    <img src={reactLogo} className="App-logo" alt="react logo" />
   </footer>
 );
 
